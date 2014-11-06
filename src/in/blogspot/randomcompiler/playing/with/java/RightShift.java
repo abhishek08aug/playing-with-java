7116c00f6c773f3e405fd96b75cacf954854fc30
abhishek.s <abhishek.s@247-inc.com>
2014-11-06 17:28:11 +0530

    right shift test

---
+++ src/in/blogspot/randomcompiler/playing/with/java/RightShift.java
@@ -0,0 +1,46 @@
+package in.blogspot.randomcompiler.playing.with.java;
+
+public class RightShift {
+    public static void main(String args[])  {
+        int x = -4;
+        System.out.println(x>>1);   
+        int y = 4;
+        System.out.println(y>>1);   
+        
+        int a = -1;
+        System.out.println(a>>1);  
+        int b = -1;
+        System.out.println(b>>>1); 
+        System.out.println(b>>>2); 
+        System.out.println(b>>>3); 
+        System.out.println(b>>>4); 
+        System.out.println(b>>>5); 
+        System.out.println(b>>>6); 
+        System.out.println(b>>>7); 
+        System.out.println(b>>>8); 
+        System.out.println(b>>>9); 
+        System.out.println(b>>>10); 
+        System.out.println(b>>>11); 
+        System.out.println(b>>>12); 
+        System.out.println(b>>>13); 
+        System.out.println(b>>>14); 
+        System.out.println(b>>>15); 
+        System.out.println(b>>>16); 
+        System.out.println(b>>>17); 
+        System.out.println(b>>>18); 
+        System.out.println(b>>>19); 
+        System.out.println(b>>>20); 
+        System.out.println(b>>>21); 
+        System.out.println(b>>>22); 
+        System.out.println(b>>>23); 
+        System.out.println(b>>>24); 
+        System.out.println(b>>>25); 
+        System.out.println(b>>>26);
+        System.out.println(b>>>27); 
+        System.out.println(b>>>28); 
+        System.out.println(b>>>29); 
+        System.out.println(b>>>30); 
+        System.out.println(b>>>31); 
+        System.out.println(b>>>32); 
+     }    
+}
